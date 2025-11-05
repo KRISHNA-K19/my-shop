ab540353496539509c6b1f8736821f6c748da8d5
Krishnamoorthy.K <krishnamoorthyk.cse@gmail.com>
2025-11-05 18:54:56 +0530

    Updated filter and search functionality

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename ="/my-shop/">
       <App />
       <Toaster position="top-right" />
     </BrowserRouter>
